89a4cb2da98ebe8175d3abb6de421a303a8abbb1
Sara Cristina Ferreira <ferreirasara1501@hotmail.com>
2020-03-12 19:45:10 -0300

    Interface improvements

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from tkinter import Frame, Button, Label, Text, LEFT, RIGHT, X, N
+from tkinter import Frame, Button, Label, Text, LEFT, RIGHT, CENTER, TOP, BOTTOM, X, N, RAISED, BOTH, S
 
 class AppMain:
     def __init__(self, parent):
@@ -13,7 +13,24 @@ class AppMain:
         self.btnIdentifyRune = Button(self.sideFrame, text="Identificar Runa")
         self.btnAddRune = Button(self.sideFrame, text="Adicionar Runa")
         self.btnShowRunes = Button(self.sideFrame, text="Ver Runas")
+        self.btnQuit = Button(self.sideFrame, text="Sair", command=parent.quit)
         self.lblActions.pack()
         self.btnIdentifyRune.pack(fill=X)
         self.btnAddRune.pack(fill=X)
         self.btnShowRunes.pack(fill=X)
+        self.btnQuit.pack(fill=X)
+
+    def start(self):
+        self.lblWelcomeTitle = Label(self.mainFrame, text="Bem vindo!", font=("Arial Bold", 25))
+        self.lblWelcomeText = Label(self.mainFrame, text="Para analisar uma runa e descobrir o que ela significa, clique em Identificar Runa.", font=("Arial", 10))
+        self.lblWelcomeTitle.pack()
+        self.lblWelcomeText.pack()
+
+    def identifyRune(self):
+        pass
+
+    def addRune(self):
+        pass
+
+    def showRunes(self):
+        pass
--- src/core.py
+++ src/core.py
@@ -2,5 +2,7 @@ from tkinter import Tk
 from app import AppMain
 
 root = Tk()
+root.geometry('600x400')
 app = AppMain(root)
+app.start()
 root.mainloop()
